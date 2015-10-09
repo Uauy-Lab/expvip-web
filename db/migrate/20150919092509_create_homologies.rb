59453bcd1cf01cebe70b2c3b95494fb6e585531d
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2015-10-09 13:43:33 +0100

    Fixed case in reference

--- db/migrate/20150919092509_create_homologies.rb
+++ db/migrate/20150919092509_create_homologies.rb
@@ -1,9 +1,9 @@
 class CreateHomologies < ActiveRecord::Migration
   def change
     create_table :homologies do |t|
-      t.references :Gene, index: true, foreign_key: true
-      t.integer :Group
-      t.string :Genome
+      t.references :gene, index: true, foreign_key: true
+      t.integer :group
+      t.string :genome
       t.belongs_to :A, :class_name => "Gene"
   	  t.belongs_to :B, :class_name => "Gene"
   	  t.belongs_to :D, :class_name => "Gene"
