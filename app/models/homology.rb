e38709cf6b302eb6470a4fb3fcc0fffcd3ef14e7
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2015-10-12 13:08:24 +0100

    Patch to load homologies with lowercase

--- app/models/homology.rb
+++ app/models/homology.rb
@@ -1,5 +1,5 @@
 class Homology < ActiveRecord::Base
-  belongs_to :Gene
+  belongs_to :gene
   belongs_to :A, :class_name => "Gene"
   belongs_to :B, :class_name => "Gene"
   belongs_to :D, :class_name => "Gene"
