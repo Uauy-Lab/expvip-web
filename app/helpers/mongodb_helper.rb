815e3849b88b6ee8a2bb81092df986f0a0d4d6d6
Ricardo H. Ramirez-Gonzalez <ricardo.ramirez-gonzalez@jic.ac.uk>
2018-03-26 16:59:55 +0100

    Added code to read mongo configuration from mongoid.yml

--- app/helpers/mongodb_helper.rb
+++ app/helpers/mongodb_helper.rb
@@ -1,8 +1,23 @@
 require 'mongo'
 module MongodbHelper
+	def self.getConnectionConfiguration
+		client_path = Rails.root.join('config', 'mongoid.yml')
+		config_mongo = YAML.load_file(client_path)
+		opts = config_mongo[ENV['RAILS_ENV']]["clients"]["default"]
+		opts["options"]["database"] = opts["database"]
+		puts "Getting preferences. "
+		opts
+	end
+
 	def self.getConnection
+		return @client if @client
 		#This needs to be improved to be dynamic from the mongoid.yml
-		client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => "testdb")
-		client
+		mongo_config = getConnectionConfiguration
+		@client = Mongo::Client.new(
+			mongo_config["hosts"],
+			mongo_config["options"].to_options
+			)
+		#puts client.inspect
+		@client
 	end
 end
