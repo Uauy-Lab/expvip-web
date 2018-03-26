f404efbfb6fa05a4a8d93d69a87cd9edd261101c
Ricardo H. Ramirez-Gonzalez <ricardo.ramirez-gonzalez@jic.ac.uk>
2018-03-26 17:01:41 +0100

    Removed some comments

--- app/helpers/mongodb_helper.rb
+++ app/helpers/mongodb_helper.rb
@@ -5,19 +5,16 @@ module MongodbHelper
 		config_mongo = YAML.load_file(client_path)
 		opts = config_mongo[ENV['RAILS_ENV']]["clients"]["default"]
 		opts["options"]["database"] = opts["database"]
-		puts "Getting preferences. "
 		opts
 	end
 
 	def self.getConnection
 		return @client if @client
-		#This needs to be improved to be dynamic from the mongoid.yml
 		mongo_config = getConnectionConfiguration
 		@client = Mongo::Client.new(
 			mongo_config["hosts"],
 			mongo_config["options"].to_options
 			)
-		#puts client.inspect
 		@client
 	end
 end
