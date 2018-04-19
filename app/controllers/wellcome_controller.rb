f6cc5a3c176db834f7b77493e1670139558a7a75
Bijan <bijan.ghasemiafshar@gmail.com>
2018-04-19 17:05:10 +0100

    Fix the bug with the session and gene set id

--- app/controllers/wellcome_controller.rb
+++ app/controllers/wellcome_controller.rb
@@ -4,8 +4,10 @@ class WellcomeController < ApplicationController
   	 puts "This is the gene set id: #{session[:gene_set_id]}"
   	 if params[:gene_set_selector]
   	 	@gene_example_1 = Gene.where(gene_set_id: params[:gene_set_selector]).first(2) 
-  	 else
+  	 elsif session[:gene_set_id]
   	 	@gene_example_1 = Gene.where(gene_set_id: session[:gene_set_id]).first(2) 
+  	 else
+  	 	@gene_example_1 = Gene.where(gene_set_id: 1).first(2) 
   	 end  	 
 
 	respond_to do |format|
