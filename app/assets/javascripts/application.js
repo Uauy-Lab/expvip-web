6d47b0b733a1e8c89433cdf102e66c1da0dc5ca2
Bijan <bijan.ghasemiafshar@gmail.com>
2019-03-20 09:51:54 +0000

    Displays the 5 gene examples

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -63,10 +63,11 @@ function updateExample() {
       },
       success: function (response) {         
         $('#gene-search-input').attr("placeholder", response.search.gene);
-        $('#example1').html(response.search.gene);
-        $('#example2').html(response.compare.gene ); 
-        $('#example3').html(response.search.name);        
-        $('#example4').html(response.compare.name );                     
+        $('#example1').html(response.search_1.gene);
+        $('#example2').html(response.compare_1.gene );
+        $('#example3').html(response.search_2.gene);
+        $('#example4').html(response.compare_2.gene );
+        $('#example5').html(response.search_3.gene );                     
         $("select[name*='gene_set_selector']").val(geneID)
       },
       error: function(){
