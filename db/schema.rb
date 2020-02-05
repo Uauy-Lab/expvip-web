6da7ae10ab68f76f920837a0a9956f8c08655e41
TMAdams <thomas.adams@jic.ac.uk>
2020-02-05 10:26:17 +0000

    Modifies code to match current development environment

--- db/schema.rb
+++ db/schema.rb
@@ -10,34 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190327114926) do
+ActiveRecord::Schema.define(version: 2019_03_27_114926) do
 
-  create_table "ExperimentGroups_Factors", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "ExperimentGroups_Factors", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "ExperimentGroup_id", null: false
     t.integer "Factor_id", null: false
   end
 
-  create_table "default_factor_orders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "default_factor_orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.integer "order"
     t.integer "selected"
   end
 
-  create_table "experiment_groups", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "experiment_groups", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "experiment_groups_experiments", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "experiment_groups_experiments", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "experiment_group_id", null: false
     t.integer "experiment_id", null: false
     t.index ["experiment_group_id"], name: "index_experiment_groups_experiments_on_experiment_group_id"
     t.index ["experiment_id"], name: "index_experiment_groups_experiments_on_experiment_id"
   end
 
-  create_table "experiments", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "experiments", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "accession"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -48,30 +48,30 @@ ActiveRecord::Schema.define(version: 20190327114926) do
     t.index ["study_id"], name: "index_experiments_on_study_id"
   end
 
-  create_table "experiments_factors", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "experiments_factors", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "experiment_id", null: false
     t.bigint "factor_id", null: false
     t.index ["experiment_id", "factor_id"], name: "index_experiments_factors_on_experiment_id_and_factor_id"
   end
 
-  create_table "expression_bias", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "expression_bias", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name"
     t.integer "order"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "expression_bias_values", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "expression_bias_values", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.integer "decile"
-    t.float "min", limit: 24
-    t.float "max", limit: 24
+    t.float "min"
+    t.float "max"
     t.bigint "expression_bias_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["expression_bias_id"], name: "index_expression_bias_values_on_expression_bias_id"
   end
 
-  create_table "expression_values", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "expression_values", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "gene_id"
     t.integer "meta_experiment_id"
     t.integer "type_of_value_id"
@@ -82,7 +82,7 @@ ActiveRecord::Schema.define(version: 20190327114926) do
     t.index ["type_of_value_id"], name: "index_expression_values_on_type_of_value_id"
   end
 
-  create_table "factors", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "factors", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.integer "order"
@@ -92,7 +92,7 @@ ActiveRecord::Schema.define(version: 20190327114926) do
     t.index ["factor"], name: "index_factors_on_factor"
   end
 
-  create_table "gene_sets", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "gene_sets", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
@@ -100,7 +100,7 @@ ActiveRecord::Schema.define(version: 20190327114926) do
     t.boolean "selected"
   end
 
-  create_table "genes", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "genes", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "cdna"
     t.string "possition"
@@ -116,7 +116,7 @@ ActiveRecord::Schema.define(version: 20190327114926) do
     t.index ["transcript"], name: "index_genes_on_transcript"
   end
 
-  create_table "homologies", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "homologies", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "gene_id"
     t.integer "group"
     t.string "genome"
@@ -128,7 +128,7 @@ ActiveRecord::Schema.define(version: 20190327114926) do
     t.index ["gene_id"], name: "index_homologies_on_gene_id"
   end
 
-  create_table "homology_pairs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "homology_pairs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "homology"
     t.string "cigar"
     t.decimal "perc_cov", precision: 7, scale: 4
@@ -141,12 +141,12 @@ ActiveRecord::Schema.define(version: 20190327114926) do
     t.index ["homology"], name: "index_homology_pairs_on_homology"
   end
 
-  create_table "links", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "links", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "url"
     t.string "site_name"
   end
 
-  create_table "meta_experiments", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "meta_experiments", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.integer "gene_set_id"
@@ -156,7 +156,7 @@ ActiveRecord::Schema.define(version: 20190327114926) do
     t.index ["name"], name: "index_meta_experiments_on_name"
   end
 
-  create_table "sample_genes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "sample_genes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "gene_set_id"
     t.integer "gene_id"
     t.string "kind"
@@ -164,7 +164,7 @@ ActiveRecord::Schema.define(version: 20190327114926) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "sessions", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "sessions", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "session_id", null: false
     t.text "data"
     t.datetime "created_at"
@@ -173,14 +173,14 @@ ActiveRecord::Schema.define(version: 20190327114926) do
     t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
-  create_table "species", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "species", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "scientific_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "studies", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "studies", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "accession"
     t.string "title"
     t.string "manuscript", limit: 500
@@ -199,14 +199,14 @@ ActiveRecord::Schema.define(version: 20190327114926) do
     t.index ["title"], name: "index_studies_on_title"
   end
 
-  create_table "tissues", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "tissues", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "type_of_values", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "type_of_values", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
@@ -214,7 +214,7 @@ ActiveRecord::Schema.define(version: 20190327114926) do
     t.index ["name"], name: "index_type_of_values_on_name"
   end
 
-  create_table "varieties", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "varieties", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.string "url"
@@ -222,7 +222,6 @@ ActiveRecord::Schema.define(version: 20190327114926) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "experiments", "studies"
   add_foreign_key "expression_bias_values", "expression_bias", column: "expression_bias_id"
   add_foreign_key "expression_values", "genes"
   add_foreign_key "expression_values", "meta_experiments"
